e4ad8aead1d810178ac2c4eb64fb6778162c09a7
Christopher J. Brody <brodybits@litehelpers.net>
2015-11-02 13:41:39 +0100

    Initial Commit

---
+++ qsa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qsa
+//
+//  Created by C. Brody on 11/2/15.
+//  Copyright (c) 2015 C. Brody. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qsa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qsa
+//
+//  Created by C. Brody on 11/2/15.
+//  Copyright (c) 2015 C. Brody. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
