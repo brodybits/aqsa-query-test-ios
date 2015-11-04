60a9451e48999a30347d04cfa28cea7b8ca42dc7
Christopher J. Brody <brodybits@litehelpers.net>
2015-11-04 17:43:48 +0100

    Split req URI into components & show results

--- qsa/MyURLProtocol.m
+++ qsa/MyURLProtocol.m
@@ -14,13 +14,41 @@ extern UIWebView *gWebView;
 + (BOOL) canInitWithRequest:(NSURLRequest *)request {
     NSLog(@"got uri: %@", request.URL.absoluteString);
     if ([request.URL.absoluteString hasPrefix:@"file:///aqaq"]) {
-      NSString * s1 = [NSString stringWithFormat: @"got uri: %@", request.URL.absoluteString];
-      NSString * e = [NSString stringWithFormat:@"%@('%@');", @"aqcallback", s1];
+        NSString * req = request.URL.absoluteString;
+        NSArray * topComponents = [req componentsSeparatedByString: @"#"];
+        //NSString * s1 = [NSString stringWithFormat: @"got components: %@ %@", [topComponents objectAtIndex: 0], [topComponents count] < 2 ? @"" : [topComponents objectAtIndex: 1]];
+        if ([topComponents count] < 2) {
+            NSLog(@"SORRY missing # in URI: %@", req);
+            return NO;
+        }
 
-      dispatch_async(dispatch_get_main_queue(), ^{
-        // FUTURE TBD: [myJSContext evaluateScript: e];
-        [gWebView stringByEvaluatingJavaScriptFromString: e];
-      });
+        NSString * handleString = [topComponents objectAtIndex: 1];
+        NSArray * handleComponents = [handleString componentsSeparatedByString: @"?"];
+        if ([handleComponents count] < 2) {
+            NSLog(@"SORRY missing ? in URI: %@", req);
+            return NO;
+        }
+
+        NSArray * routeComponents = [[handleComponents objectAtIndex: 0] componentsSeparatedByString: @":"];
+        if ([routeComponents count] < 2) {
+            NSLog(@"SORRY missing : in URI: %@", req);
+            return NO;
+        }
+
+        NSArray * routeParamComponents = [[routeComponents objectAtIndex: 1] componentsSeparatedByString: @"@"];
+        if ([routeParamComponents count] < 2) {
+            NSLog(@"SORRY missing @ in URI: %@", req);
+            return NO;
+        }
+
+        NSString * s1 = [NSString stringWithFormat: @"got obj: %@ method: %@ code: %@ params: %@", [routeComponents objectAtIndex: 0], [routeParamComponents objectAtIndex: 0], [routeParamComponents objectAtIndex: 1], [handleComponents objectAtIndex: 1]];
+
+        NSString * myScript = [NSString stringWithFormat:@"%@('%@');", @"aqcallback", s1];
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // FUTURE TBD: [myJSContext evaluateScript: myScript];
+            [gWebView stringByEvaluatingJavaScriptFromString: myScript];
+        });
     }
     return NO;
 }
--- www/mytask.js
+++ www/mytask.js
@@ -1,6 +1,6 @@
 // THANKS: http://www.html5rocks.com/en/tutorials/workers/basics/
 self.addEventListener('message', function(ev) {
   var r = new XMLHttpRequest();
-  r.open("POST", "file:///aqaqaq/asdf?fsd=wlfk%23&fw=%4238s@fda", true);
+  r.open("POST", "file:///aqaq#as:df@a1?fs,d=wl%23&fw=%4238s@fda", true);
   r.send();
 });
